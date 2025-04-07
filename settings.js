369cb29661cfe8f49322b914994cf8e77e03ad3f
Sung544 <onyedikachionuchukwu17@gmail.com>
2025-04-07 12:39:30 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: '『♤ᴸᴱᴳᴱᴺᴰ╳🅽Δ🅶🅸♤』',
+  author: '『♤ᴸᴱᴳᴱᴺᴰ╳🅽Δ🅶🅸♤』',
+  botName: "♤🅽Δ🅶🅸",
+  botOwner: '『♤ᴸᴱᴳᴱᴺᴰ╳🅽Δ🅶🅸♤』', // Your name
+  ownerNumber: '27699938662', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
